495ea97ca7e9cd3b2dd2da3ad1cc2abe02b6b348
Susmit Mukherjee <75859651+SusmitM@users.noreply.github.com>
2024-11-28 21:55:19 +0530

    Feat: Messages Route

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,106 @@
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession, User } from "next-auth";
+import { NextRequest } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/options";
+import UserModel from "@/model/User";
+
+export const POST = async (request: NextRequest) => {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+
+  const user = session?.user as User;
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessage: acceptMessages },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Failed to updated user status to accept messages",
+        },
+        { status: 401 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "Msg acceptance status updated successfully",
+        updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Failed to updated user status to accept messages",error);
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to updated user status to accept messages",
+      },
+      { status: 500 }
+    );
+  }
+};
+
+
+
+export const GET=async(request:Request)=>{
+    await dbConnect()
+    const session = await getServerSession(authOptions);
+    const user = session?.user as User;
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+  const userId = user._id;
+  try{
+    const foundUser = await UserModel.findById(userId) as User;
+    if(!foundUser) {
+        // User not found
+        return Response.json(
+          { success: false, message: 'User not found' },
+          { status: 404 }
+        );
+      }
+  
+      // Return the user's message acceptance status
+      return Response.json(
+        {
+          success: true,
+          isAcceptingMessages: foundUser.isAcceptingMessages
+        },
+        { status: 200 }
+      );
+
+  }
+  catch(error){
+    console.error("Error in getting the user message status",error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error in getting the user message status",
+      },
+      { status: 500 }
+    );
+  }
+
+}
---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,49 @@
+import dbConnect from "@/lib/dbConnect"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../auth/[...nextauth]/options"
+import mongoose from "mongoose"
+import UserModel from "@/model/User"
+
+export const GET= async(request:Request)=>{
+   await dbConnect()
+
+   const session=await getServerSession(authOptions);
+   const user =session?.user;
+
+   if(!session || !user){
+    return Response.json({success:false,message:"Not Authenticated"},{status:401})
+   }
+
+   const userId=new mongoose.Types.ObjectId(user?._id); 
+
+   try{
+    const user=await UserModel.aggregate([
+        {$match:{id:userId}},
+        {$unwind:'$messages'},
+        {$sort:{'messages.createdAt':-1}},
+        {$group:{_id:'$_id',messages:{$push:'$messages'}}}
+    ])
+
+    if(!user || user.length===0){
+        return Response.json({
+            success:false,
+            message:'User not found'
+        },
+    {status:401})
+    }
+    return Response.json({
+        success:true,
+        message:user[0]?.messages
+    },
+{status:200})
+   }
+   catch(error){
+    console.error("Error to get message",error)
+    return Response.json({
+        success:false,
+        message:'Internal Server Error'
+    },
+{status:500})
+}
+
+}
